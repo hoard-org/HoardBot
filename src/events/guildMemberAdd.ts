68bb7d63f4262c0cbe7c1b04604a70a2c18760db
LandenStephenss <turtlederp806@icloud.com>
2022-07-06 16:54:18 -0500

    'lint'

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -54,7 +54,7 @@ export default class guildMemberAdd extends Event {
   async run(guild: Guild, member: Member) {
     if(guild.id === process.env['HRD_ID']) {
 
-        this.client.createMessage(
+      this.client.createMessage(
         process.env['HRD_JOIN_LEAVE']!, 
         Messages[Math.floor(Math.random() * Messages.length)]
           .replace('{user}', `<@${member.id}>`)
